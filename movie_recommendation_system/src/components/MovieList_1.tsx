6762d7e4ff08fc852988ed46fab0e0ba35066222
DrGnNatsu <phphuong.career@gmail.com>
2025-04-21 23:32:46 +0700

    Phuong | Init Movie List (ID = 1)

---
+++ movie_recommendation_system/src/components/MovieList_1.tsx
@@ -0,0 +1,10 @@
+export default function MovieList_1() {
+    return (
+        <div
+            className="mt-8 mb-8 ml-16 mr-16
+                    grid grid-cols-5 gap-12
+                    bg-black">
+            
+        </div>
+    );
+}
--- movie_recommendation_system/src/types/I_MovieList1.ts
+++ movie_recommendation_system/src/types/I_MovieList1.ts
@@ -1,4 +1,3 @@
 export default function I_MovieList1() {
-    
 
 }
