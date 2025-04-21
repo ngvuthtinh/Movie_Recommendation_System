dc2b7dd656c1d619dabf2ad73715090ba8786c21
DrGnNatsu <phphuong.career@gmail.com>
2025-04-21 18:52:39 +0700

    Phuong | Finish MovieList ID=3

--- movie_recommendation_system/src/App.tsx
+++ movie_recommendation_system/src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css'
+import MovieList_3 from './components/MovieList_3.tsx'
 
 function App() {
   return (
-    <></>
+    <>
+      <MovieList_3 />
+    </>
   )
 }
 
---
+++ movie_recommendation_system/src/types/Poster.ts
@@ -0,0 +1,6 @@
+export interface Poster {
+    id: number;
+    title: string;
+    description: string;
+    poster_path: string;
+}
