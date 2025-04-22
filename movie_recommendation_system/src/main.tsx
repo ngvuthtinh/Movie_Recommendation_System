97b2f11a5acf12f83fc9e233aec28c7dd8ffbecf
Nguyen Vu Thanh Tinh <ITCSIU23039@student.hcmiu.edu.vn>
2025-04-22 12:36:46 +0700

    Tinh | Init Detail Movie (ID = 1)

--- movie_recommendation_system/src/main.tsx
+++ movie_recommendation_system/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.tsx'
+import DetailMovie_1 from './components/DetailMovie_1.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <DetailMovie_1 />
   </StrictMode>,
 )
