6494cc0b5f81c1038969ea6b030d3719e6e2f419
DrGnNatsu <phphuong.career@gmail.com>
2025-04-24 21:38:14 +0700

    Phuong | Format<app>

--- movie_recommendation_system/src/App.tsx
+++ movie_recommendation_system/src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-import MovieList2 from "./components/movie_list_2.tsx";
-import MovieList1 from "./components/movie_list_1.tsx";
+
 
 function App() {
   return (
     <>
-        <MovieList2 />
-        <MovieList1 />
+        
     </>
   )
 }
