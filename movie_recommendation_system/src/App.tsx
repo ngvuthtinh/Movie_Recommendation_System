6998957c15ac5b2990e6e7053eb7ee93a5a9f374
PhamAnhKhoi10 <ITCSIU23018@student.hcmiu.edu.vn>
2025-04-30 16:48:03 +0700

    Khoi | merge to dev

--- movie_recommendation_system/src/App.tsx
+++ movie_recommendation_system/src/App.tsx
@@ -1,15 +1,8 @@
 import './styles/App.css'
-import DetailMovie_1 from './components/DetailMovie_1'
-import DetailMovie_2 from './components/DetailMovie_2'
+
 function App() {
   return (
     <>
-      <div className="mb-10">
-        <DetailMovie_1 />
-      </div>
-      <div className="mt-10">
-        <DetailMovie_2 />
-      </div>
     </>
   )
 }
