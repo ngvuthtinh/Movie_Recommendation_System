e119232a0839cd2f4f1c5118971357807dae9b02
DrGnNatsu <phphuong.career@gmail.com>
2025-05-02 11:07:18 +0700

    Phuong | fix<components>: redesign nav after login
    - Notes: Need the form for the clicked to view the profile, sign out, ...
    - Reformat all the file
    - Change to UI library

--- movie_recommendation_system/src/components/nav_afterLogin.tsx
+++ movie_recommendation_system/src/components/nav_afterLogin.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { SiNetflix } from "react-icons/si";
 import { FiBell, FiSearch } from 'react-icons/fi';
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
 
 const NavAfterLogin : React.FC = () => {
     // Handle draw rectangle
 
     return(
-        <nav className="bg-black text-white flex justify-between items-center">
+        <nav className="bg-black flex justify-between items-center ">
             {/* Logo and navigation button */}
-            <div className="flex gap-4 items-center m-4">
-                <SiNetflix className="text-red-600 size-8" />
+            <div className="flex gap-4 items-center mr-4 ml-4">
+                <SiNetflix className="text-red-600 size-9" />
 
                 <Button className="bg-transparent rounded-none hover:bg-red-600 hover:rounded-b-lg">
                     Home
@@ -25,24 +26,25 @@ const NavAfterLogin : React.FC = () => {
                 </Button>
             </div>
 
-
             {/* Search bar, Icons and Avatar */}
-            <button className='flex items-center gap-4'>
-            <div className="relative">
-                    <input
-                        type="text"
-                        placeholder="Hinted search text"
-                        className="bg-zinc-800 pl-4 pr-10 py-1 rounded-full text-white placeholder-gray-400"
-                    />
-                    <FiSearch className="absolute right-2 top-1/2 transform -translate-y-1/2 text-white text-lg pointer-events-none" />
-            </div>
-            <FiBell className='text-white text-lg cursor-pointer'/>
+            <div className="flex items-center gap-4 mr-4 ml-4">
+                <div className="relative">
+                        <Input
+                            type="text"
+                            placeholder="Hinted search text"
+                            className="bg-white text-black rounded-full
+                                    placeholder-gray-400 px-6"
+                        />
+                        <FiSearch className="absolute right-4 top-1/2 transform -translate-y-1/2 text-lg text-black" />
+                </div>
+                <FiBell className="text-white cursor-pointer size-5
+                                    hover:background-zinc-500 hover:rounded-full" />
                 <img
                     src="https://i.pravatar.cc/32"
-                    className='rounded-full w-8 h-8 object-cover'
+                    className="rounded-full size-9 object-cover"
                     alt="Use Avatar"
-                    />
-            </button>
+                />
+            </div>
         </nav>
     );
 }
