98a596e98c2d71987e39e0272ace284cdba87499
PhamAnhKhoi10 <ITCSIU23018@student.hcmiu.edu.vn>
2025-05-04 23:32:58 +0700

    Khoi | Finish merging components

--- movie_recommendation_system/src/App.tsx
+++ movie_recommendation_system/src/App.tsx
@@ -1,14 +1,8 @@
 import './styles/App.css'
-import NavBeforeLogin from "@/components/nav_beforeLogin.tsx";
-import NavAfterLogin from "@/components/nav_afterLogin.tsx";
-import Footer from "@/components/footer.tsx";
 
 function App() {
   return (
     <>
-        <NavBeforeLogin />
-        <NavAfterLogin />
-        <Footer />
     </>
   )
 }
