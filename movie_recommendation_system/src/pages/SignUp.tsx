3c7c7f42db3116f30de1772312e4fc13c95db1f7
PhamAnhKhoi10 <ITCSIU23018@student.hcmiu.edu.vn>
2025-05-05 20:32:50 +0700

    Khoi | feat: update sign up page

--- movie_recommendation_system/src/pages/SignUp.tsx
+++ movie_recommendation_system/src/pages/SignUp.tsx
@@ -10,7 +10,7 @@ export default function SignUp() {
             </div>
 
             <div className="bg-[url(/src/assets/backgrounds/sign-up-bg.jpg)] bg-cover bg-center relative flex items-center justify-center pt-25 pb-10">
-                <div className="w-full max-w-[500px] p-9 bg-black/70 rounded-lg shadow-lg">
+                <div className="w-full max-w-[500px] p-9 bg-black/70 rounded-xl shadow-lg">
                     <SignupForm />
                 </div>
             </div>
